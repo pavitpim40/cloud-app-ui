20bed25bbf7c6c85ba7431143cc6e0bfd5b1ed96
Pavit <pavitpim@gmail.com>
2022-06-04 21:08:25 +0700

    about

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About"
 function App() {
   return (
     <>
       <Navbar/>
       <Hero/>
+      <About/>
     </>
   );
 }
