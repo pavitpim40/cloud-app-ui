29d34177a60bb6b7332855a92841c614ea51a7dc
pavitpim40 <pavitpim@gmail.com>
2022-06-04 23:46:35 +0700

    support section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
           </button>
           <button className="px-6 py-2">Sign Up</button>
         </div>
-        <div className="md:hidden" onClick={handleClick}>
+        <div className="md:hidden mr-4" onClick={handleClick}>
           {!nav ? <MenuIcon className="w-5" /> : <XIcon className="w-5" />}
         </div>
       </div>
@@ -34,7 +34,7 @@ const Navbar = () => {
         <li className="border-b-2 border-zinc-300 w-full">Pricing</li>
         <div className="flex flex-col my-1">
             <button className="bg-transparent text-indigo-600 px-8 py-3 mb-4">Sign In</button>
-            <button className="px-8 py-1 mb-4">Sign Up</button>
+            <button className="px-8 py-3 mb-4">Sign Up</button>
         </div>
       </ul>
     </div>
